d59da2c74ee1fc4f5c836d5c509cf7732790836c
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-04-24 19:23:34 +0200

    init project reactive mongodb crud

---
+++ sb-reactive-crud-mongodb/src/main/java/io/michaeljgkopp/github/sbreactivecrudmongodb/SbReactiveCrudMongodbApplication.java
@@ -0,0 +1,13 @@
+package io.michaeljgkopp.github.sbreactivecrudmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbReactiveCrudMongodbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbReactiveCrudMongodbApplication.class, args);
+  }
+
+}
---
+++ sb-reactive-crud-mongodb/src/test/java/io/michaeljgkopp/github/sbreactivecrudmongodb/SbReactiveCrudMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.michaeljgkopp.github.sbreactivecrudmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbReactiveCrudMongodbApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
